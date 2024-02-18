a1e11c4432f895a8c0e5c9d7ad57f9a0385c71f2
Victor Guimarães <victorfelipeguimaraes@hotmail.com>
2024-02-18 08:21:54 +0000

    Add SQLite to save quotation

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/vgmrs/goexpert-client-server-api
 go 1.21.5
 
 require github.com/go-chi/chi v1.5.5
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- server/server.go
+++ server/server.go
@@ -1,46 +1,112 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func getQuotation(w http.ResponseWriter, r *http.Request) {
+func getQuotation() (map[string]interface{}, error) {
 	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 	res, err := http.Get(url)
 	if err != nil {
 		log.Println("Error getting quote:", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Println("Error reading response:", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 
 	var result map[string]interface{}
 
 	if err := json.Unmarshal(body, &result); err != nil {
 		log.Println("Error formatting response:", err)
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func getDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Println("Error on open database connection:", err)
+		return nil, err
+	}
+	defer db.Close()
+
+	return db, err
+}
+
+func createTable(db *sql.DB) error {
+	_, err := db.Exec(`
+	CREATE TABLE IF NOT EXISTS quotation (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		quotation TEXT
+	)
+	`)
+	if err != nil {
+		log.Println("Error on create table:", err)
+		return err
+	}
+
+	return nil
+}
+
+func registerQuotation(quotation map[string]interface{}) error {
+	db, err := getDB()
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("INSERT INTO quotation (quotation) VALUES (?)", quotation)
+	if err != nil {
+		log.Println("Error when inserting data into table:", err)
+		return err
+	}
+
+	log.Println("Data saved successfully!")
+
+	return nil
+}
+
+func handlerQuotation(w http.ResponseWriter, r *http.Request) {
+	quotation, err := getQuotation()
+	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
+	registerQuotation(quotation)
+
 	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(result)
+	json.NewEncoder(w).Encode(quotation)
+}
+
+func init() {
+	db, err := getDB()
+	if err != nil {
+		log.Fatal("Error opening database connection:", err)
+	}
+
+	err = createTable(db)
+	if err != nil {
+		log.Fatal("Error creating table:", err)
+	}
 }
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/cotacao", getQuotation)
+	r.Get("/cotacao", handlerQuotation)
 
 	log.Println("Start server...")
 	http.ListenAndServe(":8080", r)
